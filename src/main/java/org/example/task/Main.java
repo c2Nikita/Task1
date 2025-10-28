db8f39bec841a073bc583b4b2bd283edae3d78a8
nikita <nikitavelic962@gmail.com>
2025-10-28 19:20:51 +0300

    Fix SonarCloud

--- src/main/java/org/example/task/Main.java
+++ src/main/java/org/example/task/Main.java
@@ -1,5 +1,7 @@
 package org.example.task;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.task.entity.CustomArray;
 import org.example.task.exception.ArrayException;
 import org.example.task.parser.impl.FileLineParserImpl;
@@ -12,11 +14,12 @@ import java.util.List;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws ArrayException {
+        Logger logger = LogManager.getLogger();
         FileArrayReaderImpl fileArrayReaderService = new FileArrayReaderImpl();
         FileLineParserImpl fileLineParserService = new FileLineParserImpl();
         List<String> list = fileArrayReaderService.readFile("data/file.txt");
         int[] array = fileLineParserService.parseToCustomArrayFromStringList(list);
-        System.out.println(Arrays.toString(array));
         CustomArray customArray = CustomArray.builder().setArray(array).setId(2).build();
+        logger.info(customArray.toString());
     }
 }
--- src/main/java/org/example/task/validator/LineValidator.java
+++ src/main/java/org/example/task/validator/LineValidator.java
@@ -2,7 +2,6 @@ package org.example.task.validator;
 
 public interface LineValidator {
     String REGEX_FILE_LINE = "\\s*-?\\d+(\\s*[,;-]\\s*-?\\d+)*\\s*";
-    ;
 
     boolean isLineFromFileValid(String lineFromFile);
 }
